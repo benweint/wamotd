539a38d47bb0061f083ecef5ce28c1b04efb1bba
Ben Weintraub <benweint@gmail.com>
2023-06-10 09:08:45 -0700

    Small cleanups

--- code.py
+++ code.py
@@ -13,7 +13,6 @@ import urllib.parse
 import digitalio
 import busio
 import board
-from adafruit_epd.ssd1675 import Adafruit_SSD1675
 from adafruit_epd.ssd1680 import Adafruit_SSD1680
 from weather_graphics import Weather_Graphics
 
@@ -23,28 +22,24 @@ dc = digitalio.DigitalInOut(board.D22)
 rst = digitalio.DigitalInOut(board.D27)
 busy = digitalio.DigitalInOut(board.D17)
 
-OPEN_WEATHER_TOKEN = "xxx"
+OPEN_WEATHER_TOKEN = os.getenv('OPEN_WEATHER_TOKEN')
 
-# Use cityname, country code where countrycode is ISO3166 format.
-LOCATION = "Portland, US"
+# Format: <cityname>, <country-code>, where countrycode is ISO3166 format.
+LOCATION = os.getenv('LOCATION', default="Portland, US")
+
 DATA_SOURCE_URL = "http://api.openweathermap.org/data/2.5/weather"
 
 if len(OPEN_WEATHER_TOKEN) == 0:
-    raise RuntimeError(
-        "You need to set your token first. If you don't already have one, you can register for a free account at https://home.openweathermap.org/users/sign_up"
-    )
+    raise RuntimeError('Please set the OPEN_WEATHER_TOKEN env var.')
 
 # Set up where we'll be fetching data from
 params = {"q": LOCATION, "appid": OPEN_WEATHER_TOKEN}
 data_source = DATA_SOURCE_URL + "?" + urllib.parse.urlencode(params)
-print(data_source)
+print(f'Will use Open Weather base API URL: {DATA_SOURCE_URL}')
 
-# Initialize the Display
-display = Adafruit_SSD1680(     # Newer eInk Bonnet
-# display = Adafruit_SSD1675(   # Older eInk Bonnet
+display = Adafruit_SSD1680(
     122, 250, spi, cs_pin=ecs, dc_pin=dc, sramcs_pin=None, rst_pin=rst, busy_pin=busy,
 )
-
 display.rotation = 1
 
 gfx = Weather_Graphics(display, am_pm=True, celsius=False)
@@ -53,6 +48,7 @@ weather_refresh = None
 while True:
     # only query the weather every 10 minutes (and on first run)
     if (not weather_refresh) or (time.monotonic() - weather_refresh) > 600:
+        # TODO: exception handling
         response = urllib.request.urlopen(data_source)
         if response.getcode() == 200:
             value = response.read()
